3ca190b548b3e0cd66e85cb6ff70ce48d20b6202
Chris sayen <neuralskill@gmail.com>
2018-12-04 22:06:51 -0500

    first commit

---
+++ smartCar/smartCar.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		smartCar.ino
+ Created:	12/4/2018 10:04:22 PM
+ Author:	PitBoys
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
